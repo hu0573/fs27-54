6159c22b16e03d49c678b19e5028c0ec8c224178
Ruiqing Hu <huruiqing0011@gmail.com>
2025-11-04 13:10:17 +1030

    background

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,24 @@ import Items from "./components/Items";
 
 function App() {
   return (
-    <div
-      className="w-screen h-screen justify-items-center "
-      style={{
-        backgroundImage: "url('/images/bg.png')",
-        backgroundSize: "cover",
-      }}
-    >
-      <div className="p-5 max-w-5xl w-full h-full">
-        <div className="p-10  w-full h-full bg-white/30 rounded-4xl">
-          <h1 className="text-3xl">Notes</h1>
-          <Items></Items>
+    <>
+      <div
+        className="fixed inset-0 -z-10"
+        style={{
+          backgroundImage: "url('/images/bg.png')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      ></div>
+      <div className="flex justify-center w-full h-full">
+        <div className="relative z-10 p-5 max-w-5xl w-full min-h-screen">
+          <div className="p-10 w-full h-full bg-white/30 rounded-4xl">
+            <h1 className="text-3xl">Notes</h1>
+            <Items></Items>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
